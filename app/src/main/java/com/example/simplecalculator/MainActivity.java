5af84ddcc99087bd1c6a4cd562c30cce27507ad9
huzaifanh <47677442+huzaifanh@users.noreply.github.com>
2021-05-23 10:10:44 -0400

    Update MainActivity.java
    added onClickListeners for the rest of the number buttons
    ADDED DIRECTLY THROUGH GITHUB

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -11,12 +11,12 @@ public class MainActivity extends AppCompatActivity {
     Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnSub, btnDiv, btnMul, btnClr, btnEql, btnDec;
 
     TextView display;
-     double val1, val2;
+    double val1, val2;
 
-     boolean add,sub,mul,div, equals;
+    boolean add,sub,mul,div;
 
-     enum Operator{none, add, minus, multiply, divide}
-     Operator optr = Operator.none;
+    enum Operator{none, add, minus, multiply, divide}
+    Operator optr = Operator.none;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,47 +28,107 @@ public class MainActivity extends AppCompatActivity {
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
         btn4 = findViewById(R.id.btn4);
-        btn5 = findViewById(R.id.btn6);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
         setClickListener();
     }
-     private void setClickListener(){
+    private void setClickListener(){
         //initializes the onClick for each button, missing some buttons
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btn0.setOnClickListener(new View.OnClickListener() {
 
-        public void onClick(View v){
-            display.setText(display.getText() + "1");
+            public void onClick(View v){
+                display.setText(display.getText() + "0");
             }
         });
-         btn2.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() {
 
-             public void onClick(View v){
-                 display.setText(display.getText() + "2");
-             }
-         });
-         btn3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                display.setText(display.getText() + "1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
 
-             public void onClick(View v){
-                 display.setText(display.getText() + "3");
-             }
-         });
-         btn4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                display.setText(display.getText() + "2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
 
-             public void onClick(View v){
-                 display.setText(display.getText() + "4");
-             }
-         });
-         btn5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                display.setText(display.getText() + "3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
 
-             public void onClick(View v){
-                 display.setText(display.getText() + "5");
-             }
-         });
-         btn0.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                display.setText(display.getText() + "4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
 
-             public void onClick(View v){
-                 display.setText(display.getText() + "0");
-             }
-         });
+            public void onClick(View v){
+                display.setText(display.getText() + "5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                display.setText(display.getText() + "6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                display.setText(display.getText() + "7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                display.setText(display.getText() + "8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                display.setText(display.getText() + "9");
+            }
+        });
+        btnEql.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                MainActivity.this.equals = true ;
+                MainActivity.this.val2 = Float.parseFloat(MainActivity.this.display.getText() + " ");
+                if (MainActivity.this.add) {
+                    MainActivity.this.display.setText(MainActivity.this.val1 + " + " + MainActivity.this.val2 + " = " + (MainActivity.this.val1 + MainActivity.this.val2) + " ");
+                    MainActivity.this.add = false;
+                }
+
+                if (MainActivity.this.sub) {
+                    MainActivity.this.display.setText(MainActivity.this.val1 - MainActivity.this.val2 + " ");
+                    MainActivity.this.sub = false;
+                }
+
+                if (MainActivity.this.div) {
+                    MainActivity.this.display.setText(MainActivity.this.val1 / MainActivity.this.val2 + " ");
+                    MainActivity.this.div = false;
+                }
+
+                if (MainActivity.this.mul) {
+                    MainActivity.this.display.setText(MainActivity.this.val1 * MainActivity.this.val2 + " ");
+                    MainActivity.this.mul = false;
+                }
+
+            }
+
+        });
+
+
+
     }
     // for reference
 //    btn1.setOnClickListener(new View.OnClickListener() {
@@ -79,28 +139,7 @@ public class MainActivity extends AppCompatActivity {
 //    });
 
 
-    btnEql.setOnClickListener(){
-        MainActivity.this.equals = true ;
-        MainActivity.this.val2 = Float.parseFloat(MainActivity.this.display.getText() + " ");
-        if (MainActivity.this.add) {
-            MainActivity.this.display.setText(MainActivity.this.val1 + " + " + MainActivity.this.val2 + " = " + (MainActivity.this.val1 + MainActivity.this.val2) + " ");
-            MainActivity.this.add = false;
-        }
 
-        if (MainActivity.this.sub) {
-            MainActivity.this.display.setText(MainActivity.this.val1 - MainActivity.this.val2 + " ");
-            MainActivity.this.sub = false;
-        }
 
-        if (MainActivity.this.div) {
-            MainActivity.this.display.setText(MainActivity.this.val1 / MainActivity.this.val2 + " ");
-            MainActivity.this.div = false;
-        }
 
-        if (MainActivity.this.mul) {
-            MainActivity.this.display.setText(MainActivity.this.val1 * MainActivity.this.val2 + " ");
-            MainActivity.this.mul = false;
-        }
-
-    };
 }
