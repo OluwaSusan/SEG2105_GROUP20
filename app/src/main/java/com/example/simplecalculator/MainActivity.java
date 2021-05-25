b8ecaa336631edad1282aee485f5da77a255bac6
Susan <peters.susan99@gmail.com>
2021-05-25 10:27:52 -0400

    Crashes Fixed, Backspace Icon implemented - Susan

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -7,18 +7,20 @@ import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 //This is a test
 
 public class MainActivity extends AppCompatActivity {
-    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnSub, btnDiv, btnMul, btnClr, btnEql, btnDec, btnBksp;
-
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnSub, btnDiv, btnMul, btnClr, btnEql, btnDec, btnNeg;
+    ImageButton btnBksp;
     private EditText display;
     Double val1, val2 = new Double(0);
 
     boolean add,sub,mul,div;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         display = findViewById(R.id.input);
-
         btn0 = findViewById(R.id.btn0);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
@@ -45,14 +46,17 @@ public class MainActivity extends AppCompatActivity {
         btnClr = findViewById(R.id.btnClr);
         btnDec = findViewById(R.id.btnDec);
         btnBksp = findViewById(R.id.btnBksp);
+        btnNeg = findViewById(R.id.btnNeg);
 
 
 
         //display.setOnClickListener();
         setClickListener();
     }
+
     private void setClickListener(){
         //initializes the onClick for each button, missing some buttons
+
         btn0.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v){
@@ -62,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v){
+
                 display.setText(display.getText() + "1");
             }
         });
@@ -119,39 +124,46 @@ public class MainActivity extends AppCompatActivity {
                 display.setText(display.getText() + ".");
             }
         });
+        btnNeg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText("-" + display.getText());
+            }
+        });
         btnEql.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
 
+//              MainActivity.this.equals = true;
+                if (display.getText().toString().isEmpty()){
+                    display.setText((CharSequence)null);
+                }
+                else {
+
+                    val2 = Double.parseDouble(display.getText() + " ");
+                    double answer = 0;
+
+                    if (add) {
+                        answer = val1 + val2;
+                        add = false;
+                    } else if (sub) {
+                        answer = val1 - val2;
+                        sub = false;
+                    } else if (div) {
+                        answer = val1 / val2;
+                        div = false;
+                    } else if (mul) {
+                        answer = val1 * val2;
+                        mul = false;
+                    }
 
 
-//                MainActivity.this.equals = true;
-                val2 = Double.parseDouble(display.getText() + " ");
-                double answer = 0;
-                if (add) {
-                    answer = val1 + val2;
-                    add = false;
+                    if (answer % 1 == 0) {
+                        display.setText(Integer.toString((int) answer));
+                    } else {
+                        display.setText(Double.toString(answer));
+                    }
                 }
 
-                else if (sub) {
-                    answer = val1 - val2;
-                    sub = false;
-                }
-
-                else if (div) {
-                    answer = val1/val2;
-                   div = false;
-                }
-
-                else if (mul) {
-                    answer = val1*val2;
-                    mul = false;
-                }
-
-                if(answer%1 == 0){
-                    display.setText( Integer.toString((int) answer));
-                }else{
-                    display.setText(Double.toString(answer));
-                }
 
 
 
@@ -160,57 +172,70 @@ public class MainActivity extends AppCompatActivity {
 
         btnAdd.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                if (display == null) {
+                if (display.getText().toString().isEmpty()) {
+                    //val1 = Double.parseDouble("0");
+                    //add = true;
                     display.setText((CharSequence)null);
                 }
 
-                val1 = Double.parseDouble(display.getText() + " ");
-                add = true;
-                display.setText((CharSequence)null);
+                else {
+                    val1 = Double.parseDouble(display.getText() + " ");
+                    add = true;
+                    display.setText((CharSequence) null);
+                }
             }
                 });
         btnSub.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (display == null) {
+                if (display.getText().toString().isEmpty()) {
                     display.setText((CharSequence)null);
                 }
 
-                val1 = Double.parseDouble(display.getText() + " ");
-                sub = true;
-                display.setText((CharSequence)null);
+                else {
+                    val1 = Double.parseDouble(display.getText() + " ");
+                    sub = true;
+                    display.setText((CharSequence) null);
+                }
             }
                 });
 
         btnDiv.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                if (display == null) {
+                if (display.getText().toString().isEmpty()) {
                     display.setText((CharSequence)null);
                 }
 
-                val1 = Double.parseDouble(display.getText() + " ");
-                div = true;
-                display.setText((CharSequence)null);
+                else {
+                    val1 = Double.parseDouble(display.getText() + " ");
+                    div = true;
+                    display.setText((CharSequence) null);
+                }
             }
                 });
         btnMul.setOnClickListener(new View.OnClickListener(){
-                    public void onClick(View v) {
-                        if (display == null) {
-                            display.setText((CharSequence)null);
-                        }
+            public void onClick(View v) {
+                if (display.getText().toString().isEmpty()) {
+                    display.setText((CharSequence)null);
+                }
 
-                        val1 = Double.parseDouble(display.getText() + " ");
-                        mul = true;
-                        display.setText((CharSequence)null);
-                    }
+                else {
+                    val1 = Double.parseDouble(display.getText() + " ");
+                    mul = true;
+                    display.setText((CharSequence) null);
+                }
+            }
 
 
         });
         btnClr.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                display.setText((CharSequence)null);
-                val1 = null;
-                val2 = null;
+
+                    display.setText((CharSequence) null);
+                    //display.setText("0");
+                    val1 = null;
+                    val2 = null;
+
             }
         });
         btnBksp.setOnClickListener(new View.OnClickListener() {
