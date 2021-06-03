6b124db38aaaf8becae87b50d3dd992e60c926a0
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-06-03 19:35:26 -0400

    onCreate

--- Product_Manager_App/app/src/main/java/com/example/productmanagerapp/MyDBHandler.java
+++ Product_Manager_App/app/src/main/java/com/example/productmanagerapp/MyDBHandler.java
@@ -15,8 +15,22 @@ public class MyDBHandler extends SQLiteOpenHelper{
     private static final String COLOUMN_PRODUCTNAME = "productname";
     private static final String COLOUMN_PRICE = "price";
 
+    //constructor
 
+    public MyDBHandler (Context context){
 
+        super(context, DATABASE_NAME, factory:null, DATABASE_VERSION);
 
+    }
 
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+        String CREATE_PRODUCTS_TABLE = "CREATE TABLE" + TABLE_PRODUCTS + "(" + COLUMN_ID +
+                " INTEGER PRIMARY KEY," + COLOUMN_PRODUCTNAME + "TEXT," + COLUMN_PRICE + " DOUBLE" +
+                ")";
+
+        db.execSQL(CREATE_PRODUCTS_TABLE);
+
+    }
 }
