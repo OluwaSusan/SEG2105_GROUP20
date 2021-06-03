cfe90f44ce37b67382a1814b98819745c64675bb
Susan <peters.susan99@gmail.com>
2021-06-03 19:56:41 -0400

    Main Activity Update 3

--- Product_Manager_App/app/src/main/java/com/example/productmanagerapp/MainActivity.java
+++ Product_Manager_App/app/src/main/java/com/example/productmanagerapp/MainActivity.java
@@ -31,5 +31,28 @@ public class MainActivity extends AppCompatActivity {
         //get product name from the text box
         //use the constructor from Product.java
         Product product = new Product(ProductBox.getText().toString(), price);
+
+        //add to database with the addProduct() method from MyDBHandler.java
+        dbHandler.addProduct(product);
+
+        //clear the text boxes
+        productBox.setText("");
+        priceBox.setText("");
+    }
+
+    public void lookupProduct (View view){
+
+        MyDBHandler dbHandler = new MyDBHandler(this);
+
+        //get product in the database using findProduct() method from MyDBHandler.java
+        Product product = dbHandler.findProduct(productBox.getText().toString());
+
+        //if found, then display the product details
+        //if not, display "No Match Found"
+        if(product != null){
+            idView.setText(String.valueOf(product.get_id()));
+            priceBox.setText(String.valueOf(product.get_price()));
+            
+        }
     }
 }
