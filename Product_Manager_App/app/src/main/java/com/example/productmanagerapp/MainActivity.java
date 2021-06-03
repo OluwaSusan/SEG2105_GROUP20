74d39514829ff2c816c0d84a67de1f8a5b680e7f
Susan <peters.susan99@gmail.com>
2021-06-03 19:30:26 -0400

    Main File Update 1

--- Product_Manager_App/app/src/main/java/com/example/productmanagerapp/MainActivity.java
+++ Product_Manager_App/app/src/main/java/com/example/productmanagerapp/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.productmanagerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    TextView idView;
+    EditText productBox;
+    EditText priceBox;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    idView = (TextView) findViewId(R.id.productID);
+    productBox = (EditText) findViewById(R.id.productName);
+    priceBox = (EditText) findViewById(R.id.ProductPrice);
+    
 }
