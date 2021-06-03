47bfe8082d3c41ae84c1ba22dc819e6c24ce2e6a
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-06-03 19:22:36 -0400

    imports added

--- Product_Manager_App/app/src/main/java/com/example/productmanagerapp/MyDBHandler.java
+++ Product_Manager_App/app/src/main/java/com/example/productmanagerapp/MyDBHandler.java
@@ -1,2 +1,9 @@
 package com.example.sqlitelab;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
