e33ae47684a2db20ce4fd27d840a5aaf3a61c2ae
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-06-12 10:56:26 -0400

    studentinterface added

--- Deliverable_1/app/src/main/java/com/example/course_booking_app/data/AdminInterface.java
+++ Deliverable_1/app/src/main/java/com/example/course_booking_app/data/AdminInterface.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public interface AdminInterface {
+
+
 	public void createCourse() throws IOException;
 	public void deleteCourse() throws IOException;
 	public void editCourse() throws IOException;
