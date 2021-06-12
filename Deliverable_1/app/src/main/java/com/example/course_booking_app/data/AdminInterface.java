f2e86c35aa099a9594c890353bd5690c959380ad
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-06-12 10:20:54 -0400

    Administrator.java and Admin Interface

---
+++ Deliverable_1/app/src/main/java/com/example/course_booking_app/data/AdminInterface.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface AdminInterface {
+	public void createCourse() throws IOException;
+	public void deleteCourse() throws IOException;
+	public void editCourse() throws IOException;
+
+}
---
+++ Deliverable_1/app/src/main/java/com/example/course_booking_app/data/Administrator.java
@@ -0,0 +1,13 @@
+class Administrator extends User implements AdminInterface{
+
+
+
+    public Administrator{
+        this.username = "admin"
+        this.password = "admin123"
+
+    }
+
+
+
+}
