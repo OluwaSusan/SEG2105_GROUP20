2528bd3eeeb51806a115029e96c8b839c6524256
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-06-12 14:38:11 -0400

    student.java done

---
+++ Deliverable_1/app/src/main/java/com/example/course_booking_app/data/Student.java
@@ -0,0 +1,15 @@
+package com.example.course_booking_app.data;
+
+public class Student  extends User implements StudentInterface{
+
+
+    @Override
+    public void enroll() {
+        //nothing yet
+    }
+
+    @Override
+    public void unEnroll() {
+        //nothing yet
+    }
+}
