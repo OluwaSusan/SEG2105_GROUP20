b27171bf2b736901cd4c1eb080917f3dd5e88fae
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-06-12 10:26:23 -0400

    Administrator.java update

--- Deliverable_1/app/src/main/java/com/example/course_booking_app/data/Administrator.java
+++ Deliverable_1/app/src/main/java/com/example/course_booking_app/data/Administrator.java
@@ -1,4 +1,4 @@
-class Administrator extends User implements AdminInterface{
+class Administrator extends UserType implements AdminInterface{
 
 
 
@@ -7,6 +7,22 @@ class Administrator extends User implements AdminInterface{
         this.password = "admin123"
 
     }
+
+    public void createCourse() throws IOException {
+    		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+
+    		System.out.println("Enter the Course Name: ");
+            		this.courseName = in.readLine();
+
+            		System.out.println("Enter the course ID: ");
+            		this.courseID = in.readLine();
+
+            		System.out.println("Enter the capacity for students: ");
+            		String capacity = in.readLine();
+            		int maxCap = Integer.parseInt(mS);
+
+
 
 
 
