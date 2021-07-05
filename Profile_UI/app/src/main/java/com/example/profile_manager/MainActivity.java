f734cc74e9dad3c2af0b217e02f7cc9079a9e48d
huzaifanh <47677442+huzaifanh@users.noreply.github.com>
2021-07-05 15:15:53 -0400

    fixed naming and started profile xml

--- Profile_UI/app/src/main/java/com/example/profile_manager/MainActivity.java
+++ Profile_UI/app/src/main/java/com/example/profile_manager/MainActivity.java
@@ -1,24 +1,114 @@
 package com.example.profile_manager;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button maps;
+    EditText team;
+    EditText teamAddress;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_profile);
+        this.setContentView(R.layout.activity_main);
+        maps = (Button) this.findViewById(R.id.mapsButton);
+        team = (EditText) this.findViewById(R.id.teamName_field);
+        teamAddress = (EditText) this.findViewById(R.id.address_field);
+
     }
 
-    public void SetTeamIcon(View view) {
-        Intent returnIntent = new Intent();
-        ImageView selectedImage = (ImageView)view;
-        returnIntent.putExtra("imageID", selectedImage.getId());
-        this.setResult(-1, returnIntent);
-        this.finish();
+    public void OnOpenInGoogleMaps(View view) {
+        Uri gmmIntentUri = Uri.parse("https://www.google.co.in/maps?q=" + teamAddress.getText());
+        Intent mapIntent = new Intent("android.intent.action.VIEW", gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        this.startActivity(mapIntent);
     }
+
+    ActivityResultLauncher<Intent> profileActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                    }
+                }
+            });
+
+    public void OnSetAvatarButton(View view) {
+        Intent intent = new Intent(this.getApplicationContext(), MainActivity.class);
+        profileActivityResultLauncher.launch(intent);
+    }
+
+
+    public void onActivityResult(ActivityResult result) {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+
+            Intent data = result.getData();
+            ImageView avatarImage = (ImageView) findViewById(R.id.avatarimage);
+
+            String drawableName = "flag_02";
+            switch (data.getIntExtra("imageID", R.id.flagid00)) {
+
+                case R.id.flagid00:
+                    drawableName = "flag_00";
+                    break;
+
+                case R.id.flagid01:
+                    drawableName = "flag_01";
+                    break;
+
+                case R.id.flagid02:
+                    drawableName = "flag_02";
+                    break;
+
+                case R.id.flagid03:
+                    drawableName = "flag_03";
+                    break;
+                case R.id.flagid04:
+                    drawableName = "flag_04";
+                    break;
+
+                case R.id.flagid05:
+                    drawableName = "flag_05";
+                    break;
+
+                case R.id.flagid06:
+                    drawableName = "flag_06";
+                    break;
+
+                case R.id.flagid07:
+                    drawableName = "flag_07";
+                    break;
+
+                case R.id.flagid08:
+                    drawableName = "flag_08";
+                    break;
+                default:
+                    drawableName = "flag_02";
+                    break;
+
+            }
+            int resID = getResource().getIdentifier(drawableName, "drawable", getPackageName());
+            avatarImage.setImageResource(resID);
+
+        }
+    }
+
+
+
 }
--- Profile_UI/app/src/main/java/com/example/profile_manager/ProfileActivity.java
+++ Profile_UI/app/src/main/java/com/example/profile_manager/ProfileActivity.java
@@ -18,102 +18,26 @@ import android.net.Uri;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    Button maps;
-    EditText team;
-    EditText teamAddress;
 
-
-
-    public ProfileActivity() {
-    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_main);
-        maps = (Button) this.findViewById(R.id.mapsButton);
-        team = (EditText) this.findViewById(R.id.teamName_field);
-        teamAddress = (EditText) this.findViewById(R.id.address_field);
+        this.setContentView(R.layout.activity_profile);
 
     }
 
-    public void OnOpenInGoogleMaps(View view) {
-        Uri gmmIntentUri = Uri.parse("https://www.google.co.in/maps?q=" + teamAddress.getText());
-        Intent mapIntent = new Intent("android.intent.action.VIEW", gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        this.startActivity(mapIntent);
-    }
 
-    ActivityResultLauncher<Intent> profileActivityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = result.getData();
-                    }
-                }
-            });
 
-    public void OnSetAvatarButton(View view) {
-        Intent intent = new Intent(this.getApplicationContext(), MainActivity.class);
-        profileActivityResultLauncher.launch(intent);
+
+    public void SetTeamIcon(View view) {
+        Intent returnIntent = new Intent();
+        ImageView selectedImage = (ImageView)view;
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        this.setResult(-1, returnIntent);
+        this.finish();
     }
 
 
-    public void onActivityResult(ActivityResult result) {
-        if (result.getResultCode() == Activity.RESULT_OK) {
-
-            Intent data = result.getData();
-            ImageView avatarImage = (ImageView) findViewById(R.id.avatarimage);
-
-            String drawableName = "flag_02";
-            switch (data.getIntExtra("imageID", R.id.flagid00)) {
-
-                case R.id.flagid00:
-                    drawableName = "flag_00";
-                    break;
-
-                case R.id.flagid01:
-                    drawableName = "flag_01";
-                    break;
-
-                case R.id.flagid02:
-                    drawableName = "flag_02";
-                    break;
-
-                case R.id.flagid03:
-                    drawableName = "flag_03";
-                    break;
-                case R.id.flagid04:
-                    drawableName = "flag_04";
-                    break;
-
-                case R.id.flagid05:
-                    drawableName = "flag_05";
-                    break;
-
-                case R.id.flagid06:
-                    drawableName = "flag_06";
-                    break;
-
-                case R.id.flagid07:
-                    drawableName = "flag_07";
-                    break;
-
-                case R.id.flagid08:
-                    drawableName = "flag_08";
-                    break;
-                default:
-                    drawableName = "flag_02";
-                    break;
-
-
-            }
-            int resID = getResource().getIdentifier(drawableName, "drawable", getPackageName());
-            avatarImage.setImageResource(resID);
-
-        }
-    }
 }
 
 
