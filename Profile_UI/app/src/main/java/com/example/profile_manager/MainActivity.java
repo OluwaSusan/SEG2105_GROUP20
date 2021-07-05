c87712ad44b0ed3c780722fb4937fa3d066ecf23
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-07-05 13:31:12 -0400

    lab 5 - main activity

--- Profile_UI/app/src/main/java/com/example/profile_manager/MainActivity.java
+++ Profile_UI/app/src/main/java/com/example/profile_manager/MainActivity.java
@@ -3,12 +3,76 @@ package com.example.profile_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    Button maps;
+    TextView team;
+    TextView zip;
+
+    public MainActivity() {
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setContentView(2131361820);
+        this.maps = (Button)this.findViewById(2131165342);
+        this.team = (TextView)this.findViewById(2131165422);
+        this.zip = (TextView)this.findViewById(2131165446);
     }
+
+    public void OnOpenInGoogleMaps(View view) {
+        EditText address = (EditText)this.findViewById(2131165248);
+        Uri gmmIntentUri = Uri.parse("https://www.google.co.in/maps?q=" + address.getText());
+        Intent mapIntent = new Intent("android.intent.action.VIEW", gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        this.startActivity(mapIntent);
+    }
+
+    public void OnSetAvatarButton(View view) {
+        Intent intent = new Intent(this.getApplicationContext(), ProfileActivity.class);
+        this.startActivityForResult(intent, 0);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != 0) {
+            ImageView avatarImage = (ImageView)this.findViewById(2131165265);
+            String drawableName = "ic_logo_00";
+            switch(data.getIntExtra("imageID", 2131165422)) {
+                case 2131165259:
+                    drawableName = "ic_logo_01";
+                    break;
+                case 2131165260:
+                    drawableName = "ic_logo_02";
+                    break;
+                case 2131165261:
+                    drawableName = "ic_logo_03";
+                    break;
+                case 2131165262:
+                    drawableName = "ic_logo_04";
+                    break;
+                case 2131165263:
+                    drawableName = "ic_logo_05";
+                    break;
+                case 2131165264:
+                    drawableName = "ic_logo_00";
+                    break;
+                default:
+                    drawableName = "ic_logo_00";
+            }
+
+            int resID = this.getResources().getIdentifier(drawableName, "drawable", this.getPackageName());
+            avatarImage.setImageResource(resID);
+        }
+    }
+}
+
 }
